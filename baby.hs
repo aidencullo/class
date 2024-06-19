11f3f11a43eefe3b68149a6493a3e32ae73bdb91
Aiden Cullo <aidencullo@gmail.com>
2024-06-19 09:19:16 -0400

    guards

--- baby.hs
+++ baby.hs
@@ -33,7 +33,7 @@ assert _     x = x
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x] 
 
-length' xs = sum [1 | _ <- xs] -- _ is a wildcard that matches anything and we don't care what it is
+-- length' xs = sum [1 | _ <- xs] -- _ is a wildcard that matches anything and we don't care what it is
 
 -- Just a friendly reminder: because strings are lists, we can use list comprehensions to process and produce strings. Here's a function that takes a string and removes everything except uppercase letters from it.
 
@@ -65,3 +65,82 @@ circumference r = 2 * pi * r
 
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
+
+
+-- basic pattern matching
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!" 
+
+-- Pattern matching
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum'(xs)
+
+capital :: String -> String
+capital "" = "Empty string, whoops!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+-- bmiTell :: (RealFloat a) => a -> String
+-- bmiTell bmi
+--     | bmi <= 18.5 = "You're underweight, you emo, you!"
+--     | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+--     | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+--     | otherwise   = "You're a whale, congratulations!"
+
+-- -- can't overload it seems
+-- bmiTell :: (RealFloat a) => a -> a -> String
+-- bmiTell weight height
+--     | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+--     | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+--     | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+--     | otherwise                 = "You're a whale, congratulations!"
+
+max' :: (Ord a) => a -> a -> a
+max' a b 
+    | a > b     = a
+    | otherwise = b
+
+-- max' :: (Ord a) => a -> a -> a
+-- max' a b | a > b = a | otherwise = b
+
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+-- myCompare a b
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
